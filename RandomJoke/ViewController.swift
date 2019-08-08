ffb7a81dfd9a8c33b84ee1434195a179b762a450
Ratchadaphorn Kasemphin <ratchada_may@hotmail.com>
2019-08-08 14:05:14 +0700

    Initial Commit

---
+++ RandomJoke/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomJoke
+//
+//  Created by Ratchadaphorn Kasemphin on 8/8/2562 BE.
+//  Copyright © 2562 mg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
